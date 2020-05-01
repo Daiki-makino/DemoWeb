81dc066a1723a4ac8534a91b9516c5bd02812374
Daiki Makino <daiki_makino@rosso-tokyo.co.jp>
2020-05-01 17:22:56 +0900

    データを画面表示

--- src/main/java/com/sample/ec/MainController.java
+++ src/main/java/com/sample/ec/MainController.java
@@ -1,21 +1,24 @@
 package com.sample.ec;
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller 
 public class MainController {
       @Autowired
       private BookRepository bookRepository;
-
-      @RequestMapping(value = "/")
-      public List<Book> get(){
-         return bookRepository.findAll();
+      
+      @RequestMapping(value = "/book",method = RequestMethod.GET)    
+      public String  displayList(Model model) {    
+	    List<Book> books = bookRepository.findAll();
+		model.addAttribute("books", books);   
+      return "service/book";    
       }
-
-
+      
+      
+      
 }
